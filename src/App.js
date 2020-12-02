d1b88dbb08c4e9fb05d12ea0ddb392d59b22ac4f
zero0scuro <75135817+zero0scuro@users.noreply.github.com>
2020-12-02 12:33:21 -0500

    Definiendo estructura del proyecto

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <p>
+          Hola mundo
+        </p>
+        
+      </header>
     </div>
   );
 }
